71a4e6deecfc22f90cc0b38f5ff4f6830ae1a75d
Gal Amouyal <gal.amouyal@ibm.com>
2020-11-22 19:36:16 +0200

    generate Form

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 // Globals
 const realEstates = []
 
+// const formConfiguration = [ {  id:"image", class:"form-group" ,     }  ]
+
 const DOM = {
     saveButton: document.getElementById("save_button"),
-    tableBody : document.getElementById("table_content")
+    tableBody : document.getElementById("table_content"),
+    form: document.getElementById("form_inputs")
 }
 
 
 function init(){
-    DOM.saveButton.addEventListener("click",extractDataForm)
-
+    // DOM.saveButton.addEventListener("click",extractDataForm)
+    
+    drawImageInput()
 
 }
+
+function drawImageInput(){
+    const div = document.createElement("DIV")
+    div.classList.add("form-group")
+    const label = document.createElement("LABEL")
+    label.innerText = "Image URL"
+    const input = document.createElement("INPUT")
+    input.id = "image"
+    input.type = "text";
+    input.classList.add("form-control");
+    input.placeholder = "generated image..."
+
+    div.append(label,input)
+    DOM.form.append(div)
+}
+
+
 function extractDataForm(){
     const address = document.getElementById("address").value;
     const imageURL = document.getElementById("image").value;
@@ -19,9 +40,12 @@ function extractDataForm(){
     draw(realEstates)
 }
 
-
+function clearData(){
+    DOM.tableBody.innerHTML = "";
+}
 function draw(data){
     if(!Array.isArray(data)) return; // if data is not Array return & stop function
+    clearData()
     const tableRows = data.map(realEstate=>{  return _getRow(realEstate)  })
     DOM.tableBody.append(...tableRows)
     function _getRow(realEstate){
